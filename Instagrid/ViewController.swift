135c244978954399fd1d237be6433858831490ce
Francoismdev <francoisdev@macbook-air-de-francois.home>
2023-04-25 16:08:03 +0200

    LaunchScreen appliquer

---
+++ Instagrid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by FrancoisDev on 24/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
