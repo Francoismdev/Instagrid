f4eae7bfb34341b1d01c996efed952783328895a
Francoismdev <francoismdev@icloud.com>
2023-05-05 18:26:02 +0200

    avancement button

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -9,15 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var buttonOne: UIButton!
-    @IBOutlet weak var buttonTwo: UIButton!
-    @IBOutlet weak var buttonThree: UIButton!
+    // How to open photo gallery iPhone swift UIImagePickerController
+    // let pickerController = UIImagePickerController()
+    @IBOutlet weak var topLeftView: UIView!
+    @IBOutlet weak var topRightView: UIView!
+    @IBOutlet weak var bottomLeftView: UIView!
+    @IBOutlet weak var bottomRightView: UIView!
+    
+    @IBOutlet var layoutButtons: [UIButton]!
+    
+    @IBOutlet weak var select1: UIImageView!
+    @IBOutlet weak var select2: UIImageView!
+    @IBOutlet weak var select3: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Méthode 1 : for ... in
+        for button in layoutButtons {
+            button.setTitle("", for: .normal)
+        }
+        
+        // Méthode 2 : forEach
+        layoutButtons.forEach { button in
+            button.setTitle("", for: .normal)
+        }
+        
+        // Méthode 3 : forEach avec l'utilisation de l'opérateur $0
+        layoutButtons.forEach { $0.setTitle("", for: .normal) }
+        
+        // [1, 2, 3].forEach { print($0) }
+    }
+    
+    @IBAction func layoutButtonIsPressed(_ sender: UIButton) {
+        print("button.tag: \(sender.tag)")
+        
+        let integers = [1, 2, 3]
+        
+        print("entier sélectionné: \(integers[sender.tag])")
     }
 
-
+    @IBAction func button1IsPressed(_ sender: Any) {
+        print("button 1 IsPressed")
+        // Changer le design de la vue principale
+        // Afficher l'image "Selected"
+        topLeftView.isHidden = true
+        bottomLeftView.isHidden = false
+        select1.isHidden = false
+        select2.isHidden = true
+        select3.isHidden = true
+    }
+    
+    @IBAction func button2IsPressed(_ sender: Any) {
+        print("button 2 IsPressed")
+        // Changer le design de la vue principale
+        // Afficher l'image "Selected"
+        topLeftView.isHidden = false
+        bottomLeftView.isHidden = true
+        select1.isHidden = true
+        select2.isHidden = false
+        select3.isHidden = true
+    }
+    
+    @IBAction func button3IsPressed(_ sender: Any) {
+        print("button 3 IsPressed")
+        topLeftView.isHidden = false
+        bottomLeftView.isHidden = false
+        select1.isHidden = true
+        select2.isHidden = true
+        select3.isHidden = false
+        // Changer le design de la vue principale
+        // Afficher l'image "Selected"
+    }
 }
 
