02598efc12c99fbb18b81502bbf93e576a87f31d
Francoismdev <francoismdev@icloud.com>
2023-05-05 11:59:26 +0200

    + dans images

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ Instagrid/View/ImageView.swift
@@ -0,0 +1,14 @@
+//
+//  ImageView.swift
+//  Instagrid
+//
+//  Created by FrancoisDev on 05/05/2023.
+//
+
+import UIKit
+
+class ImageView: UIView {
+
+   
+
+}
